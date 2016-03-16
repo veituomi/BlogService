2a1e5042c02a07ad779b9cc3b26b0ecac52fdd95
Tuominen Veini <veituomi@dhcp-asv-119.helsinki.fi>
2016-03-16 21:24:16 +0200

    Muutettu ympäristömuuttujia

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="kalleilv"
+USERNAME="veituomi"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+PROJECT_FOLDER="bogster"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
