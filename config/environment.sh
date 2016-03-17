f392461f3740abf42712dd66f567d5653b3ed365
Tuominen Veini <veituomi@dhcp-asv-119.helsinki.fi>
2016-03-18 00:33:35 +0200

    .gitignore is now working

--- config/environment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="USERNAME"
-# korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="PROJECT_FOLDER"
-# sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
