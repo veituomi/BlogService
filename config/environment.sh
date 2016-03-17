159885f7ca378d75b9f6df33a92f9fe4c0db2a6f
Tuominen Veini <veituomi@dhcp-asv-119.helsinki.fi>
2016-03-18 00:20:47 +0200

    Updated readme and .gitignore

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="veituomi"
+USERNAME="USERNAME"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="blogservice"
+PROJECT_FOLDER="PROJECT_FOLDER"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
