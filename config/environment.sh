c0d4e45bd82b99960a6424d28036c6da4e53ca0f
Tuominen Veini <veituomi@dhcp-asv-119.helsinki.fi>
2016-03-17 20:57:01 +0200

    Muutettu kohdehakemisto oikeaksi

--- config/environment.sh
+++ config/environment.sh
@@ -3,5 +3,5 @@
 # korvaa tämä omalla käyttäjätunnuksellasi
 USERNAME="veituomi"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="bogster"
+PROJECT_FOLDER="blogservice"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
