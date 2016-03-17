0ecb6118302e1601cacd92080d83c38db242374f
Tuominen Veini <veituomi@dhcp-asv-119.helsinki.fi>
2016-03-17 21:23:31 +0200

    Changed environment variables

--- config/environment.sh
+++ config/environment.sh
@@ -3,5 +3,5 @@
 # korvaa tämä omalla käyttäjätunnuksellasi
 USERNAME="veituomi"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="blogservice"
+PROJECT_FOLDER="BlogService"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
