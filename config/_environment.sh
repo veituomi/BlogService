5b67018b01879e94b37ce3a6bc1576addacb112c
Tuominen Veini <veituomi@dhcp-asv-119.helsinki.fi>
2016-03-18 00:50:39 +0200

    Final changes to config protection

---
+++ config/_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Poista asennuksen yhteydessä _ tiedostonimestä ja muuta tiedot ympäristöön sopiviksi
+
+# korvaa tämä omalla käyttäjätunnuksellasi
+USERNAME="USERNAME"
+# korvaa tämä haluamallasi kansion nimellä
+PROJECT_FOLDER="PROJECT_FOLDER"
+# sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
