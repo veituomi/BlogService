781cbba83d2ef6a9469c09b57021e879ea7cb534
Tuominen Veini <veituomi@dhcp-asv-119.helsinki.fi>
2016-04-04 20:39:20 +0300

    Added test data insertion

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,28 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+-- Create two users
+INSERT INTO Blogger (username, email, password) VALUES (“admin”, “admin@admin.com”, “admin”);
+INSERT INTO Blogger (username, email, password) VALUES (“sekakäyttäjä”, “user@mail.com”, “salasana”);
+
+-- Grant admin rights for the first user
+INSERT INTO Admin (userId) VALUES (1);
+
+-- Create blog and set the owner
+INSERT INTO Blog (name, description) VALUES (“Gurula kotina”, “Kertomuksia elämästä Gurulassa”);
+INSERT INTO BlogOwner (userId, blogId) VALUES (1, 1);
+
+-- Create a post for the blog
+INSERT INTO BlogPost (blogId, content) VALUES (1, “Kirjoituksen sisältö”);
+
+-- Like the post
+INSERT INTO Likes (userId, postId) VALUES (2, 1);
+
+-- Make the non-admin user follow the other
+INSERT INTO Follows (follower, followee) VALUES (2, 1);
+
+-- Create a few tags
+INSERT INTO Tag (name) VALUES (“dull”)
+INSERT INTO Tag (name) VALUES (“boring”)
+INSERT INTO Tag (name) VALUES (“blunt”)
+
+-- Link tags to the blog
+INSERT INTO TagCloud (blogId, tagId) VALUES (1, 1);
+INSERT INTO TagCloud (blogId, tagId) VALUES (1, 2);
